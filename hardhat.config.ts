e2a46eb74d5daed54fb83ca37db0f396c50494ad
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-07-17 10:02:41 +0300

    0.7

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,28 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      evmVersion: "shanghai",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAP_KEY,
+    currency: "USD",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      initialBaseFeePerGas: 0,
+    }
+  }
 };
 
 export default config;
--- test/AlbumTracker.test.ts
+++ test/AlbumTracker.test.ts
@@ -3,10 +3,9 @@ import { AlbumTracker, Album__factory } from "../typechain-types";
 import { ContractTransactionReceipt, BaseContract } from "ethers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 
-
-describe("AlbumTracker", function() {
+describe("AlbumTracker", function () {
   async function deploy() {
-    const [ owner, buyer ] = await ethers.getSigners();
+    const [owner, buyer] = await ethers.getSigners();
 
     const AlbumTracker = await ethers.getContractFactory("AlbumTracker");
 
@@ -14,12 +13,12 @@ describe("AlbumTracker", function() {
 
     await tracker.waitForDeployment();
 
-    return { tracker, owner, buyer }
+    return { tracker, owner, buyer };
   }
 
-  it("deploys albums", async function() {
+  it("deploys albums", async function () {
     const { tracker, buyer } = await loadFixture(deploy);
-    
+
     const albumTitle = "Enchantment of the Ring";
     const albumPrice = ethers.parseEther("0.00005");
 
@@ -34,7 +33,7 @@ describe("AlbumTracker", function() {
     // expect(await album.purchased()).to.eq(0);
   });
 
-  it("creates albums", async function() {
+  it("creates albums", async function () {
     const { tracker } = await loadFixture(deploy);
 
     const albumTitle = "Enchantment of the Ring";
@@ -56,12 +55,12 @@ describe("AlbumTracker", function() {
       ethers.zeroPadValue(expectedAlbumAddr, 32)
     );
 
-    await expect(receiptTx).to.emit(tracker, "AlbumStateChanged").withArgs(
-      expectedAlbumAddr, 0, 0
-    );
+    await expect(receiptTx)
+      .to.emit(tracker, "AlbumStateChanged")
+      .withArgs(expectedAlbumAddr, 0, 0, albumTitle);
   });
 
-  it("allows to buy albums", async function() {
+  it("allows to buy albums", async function () {
     const { tracker, buyer } = await loadFixture(deploy);
 
     const albumTitle = "Enchantment of the Ring";
@@ -85,52 +84,86 @@ describe("AlbumTracker", function() {
     expect((await tracker.albums(0)).state).to.eq(1);
 
     await expect(buyTx).to.changeEtherBalances(
-      [buyer, tracker], [-albumPrice, albumPrice]
+      [buyer, tracker],
+      [-albumPrice, albumPrice]
     );
 
-    await expect(
-      buyer.sendTransaction(buyTxData)
-    ).to.be.revertedWith("This album is already purchased!");
+    await expect(buyer.sendTransaction(buyTxData)).to.be.revertedWith(
+      "This album is already purchased!"
+    );
   });
 
-  it("should revert delivery f album is not paid for", async function() {
-    const { tracker, owner, buyer } = await loadFixture(deploy);
-    
+  it("should revert delivery if album is not paid for", async function () {
+    const { tracker } = await loadFixture(deploy);
+
     await tracker.createAlbum(ethers.parseEther("1"), "test");
 
-    await expect(tracker.triggerDelivery(0)).to.be.revertedWith("This album is not paid for");
+    await expect(tracker.triggerDelivery(0)).to.be.revertedWith(
+      "This album is not paid for"
+    );
   });
 
-  it("should deliver album after payment", async function() {
-    const { tracker, owner, buyer } = await loadFixture(deploy);
+  it("should deliver album after payment", async function () {
+    const { tracker, buyer } = await loadFixture(deploy);
 
     await tracker.createAlbum(ethers.parseEther("1"), "test");
-    await tracker.connect(buyer).triggerPayment(0, { value: ethers.parseEther("1")});
+    await tracker
+      .connect(buyer)
+      .triggerPayment(0, { value: ethers.parseEther("1") });
 
     await expect(tracker.triggerDelivery(0))
       .to.emit(tracker, "AlbumStateChanged")
       .withArgs(anyValue, 0, 2, "test");
   });
 
-  it("should revert if not owner tries to deliver", async function() {
-    const { tracker, owner, buyer } = await loadFixture(deploy);
+  it("should revert if not owner tries to deliver", async function () {
+    const { tracker, buyer } = await loadFixture(deploy);
 
     await tracker.createAlbum(ethers.parseEther("1"), "test");
-    await tracker.connect(buyer).triggerPayment(0, { value: ethers.parseEther("1")});
+    await tracker
+      .connect(buyer)
+      .triggerPayment(0, { value: ethers.parseEther("1") });
 
-    await expect(tracker.connect(buyer).triggerDelivery(0))
-      .to.be.revertedWith("Ownable: caller is not the owner");
+    await expect(tracker.connect(buyer).triggerDelivery(0)).to.be.revertedWith(
+      "Ownable: caller is not the owner"
+    );
   });
 
-  async function precomputeAddress(sc: BaseContract, nonce = 1): Promise<string> {
+  it("it should create new album with all proper data", async function () {
+    const album = await albumTracker.albums(1);
+
+    expect(album.title).to.eq(secondAlbumTitle);
+    expect(album.price).to.eq(secondAlbumPrice);
+    expect(album.album).to.be.properAddress;
+  });
+
+  it("should check created album has no funds", async function () {
+    const album = await albumTracker.albums(1);
+
+    const balance = await ethers.provider.getBalance(album.album);
+    expect(balance.toString()).to.eq("0");
+  });
+
+  it("should check that only owners can create albums", async function () {
+    await expect(
+      albumTracker.connect(customer).createAlbum("100", "New Titile")
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  async function precomputeAddress(
+    sc: BaseContract,
+    nonce = 1
+  ): Promise<string> {
     return ethers.getCreateAddress({
       from: await sc.getAddress(),
-      nonce
-    })
-  };
+      nonce,
+    });
+  }
 
   async function createAlbum(
-    tracker: AlbumTracker, title: string, price: bigint
+    tracker: AlbumTracker,
+    title: string,
+    price: bigint
   ): Promise<ContractTransactionReceipt | null> {
     const createAlbumTx = await tracker.createAlbum(price, title);
 
